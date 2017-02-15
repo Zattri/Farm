a4864183a71357979a4756e8b129c4094f378880
Zattri <daninjakidy@gmail.com>
2017-02-15 14:02:57 +0000

    Final commit before workshop - Added comments

--- src/Crop.java
+++ src/Crop.java
@@ -1,5 +1,7 @@
 public class Crop {
+    // Inside the brackets are the parameters you supply when creating the class
     public Crop(String cropName, int cropPrice, int growTime, int max) {
+        // Declare the object variables as the parameters from the constructor method
         name = cropName;
         price = cropPrice;
         grow = growTime;
@@ -8,22 +10,23 @@ public class Crop {
     }
 
 
-    // Variables declared in the class
-    public String name;
-    public int price;
-    public int grow;
-    public int state;
-    public int maxState;
+    // Variables initialised in the class
+    protected String name;
+    private int price;
+    private int grow;
+    private int state;
+    private int maxState;
     
 
     public void prtStats(){
-        System.out.println(this.name + "\nPrice - " + this.price + " | Grow Time - " + this.grow + " | State - " + getState() + "\n");
+        // Non-formatted - System.out.println(this.name + "\nPrice - " + this.price + " | Grow Time - " + this.grow + " | State - " + getState() + "\n");
+        System.out.format("%s - Price: %d | Grow Time: %d | State: %s\n", getName(), getPrice(), getGrowTime(), getState());
     }
 
     // Name variable functions
     public void prtName()
     {
-        System.out.println("Name - " + this.name);
+        System.out.println("Name - " + getName());
     }
     public String getName() {
         return this.name;
@@ -32,23 +35,24 @@ public class Crop {
 
     // Price variable functions
     public void prtPrice(){
-        System.out.println("Price - £" + this.price);
+        System.out.println("Price - £" + getPrice());
     }
-    public int getPrice() {
+    private int getPrice() {
         return this.price;
     }
 
 
     // Grow variable functions
     public void prtGrow() {
-        System.out.println("Grow Time - " + this.grow);
+        System.out.println("Grow Time - " + getGrowTime());
     }
-    public int getGrowTime() {
+    private int getGrowTime() {
         return this.grow;
     }
 
-
-    public String getState() {
+    // State function, private since it just returns a string we want to use in displaying the state
+    private String getState() {
+        // Declare a local variable and then modify it to reflect the state of the plant, and if it's ready
         String rtnString;
         if (this.state == this.maxState) {
             rtnString = "READY";
@@ -56,30 +60,36 @@ public class Crop {
         else {
             rtnString = this.state + "/" + this.maxState;
         }
+        // Return the local variable as an output
         return rtnString;
     }
 
     // Water plant function increases the state of the plant, unless it is maxed
     public void waterPlant() {
         if (this.state < this.maxState) {
-            this.state += 1;
+            // ++ adds one -- takes one away
+            this.state ++;
             System.out.println("Watered " + this.name + "...");
         }
     }
 }
 
+// Sub class Tree, of the super class Crop - Use extends
 class Tree extends Crop {
 
-    public int height;
+    private int height;
     public Tree(String cropName, int cropPrice, int growTime, int max, int treeHeight) {
+        // Super declares what variables are inherited from the super class in the sub class
         super(cropName, cropPrice, growTime, max);
-        name = cropName + " tree";
+        name = getName() + " tree";
         height = treeHeight;
     }
 
+    // New methods declared in Tree, based around the height variable
     public void prtHeight(){
-        System.out.print("Height = " + this.height +"m\n");
+        System.out.format("Height = %dm\n", getHeight());
     }
+    private int getHeight() {return this.height;}
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,30 @@
 import java.util.ArrayList;
 
+/*
+    A FEW THINGS TO NOTE
+    // - This is a single line comment, please read them they're helpful
+    The words function and method are inter-changeable in the way I use them (but it's best to call them methods)
+    You need public static void main(String args[]) at the beginning of any class file that you wish to be executable
+    - Put all the code that you wish to run inside it
+    It's best practice to make each separate class you use a separate class file
+    You can leave everything public if you want, it's just messier than having private variables and geter methods
+    USE GOOGLE - I can't state this enough, if you don't get something look it up
+    BE PATIENT - Following the last comment it's going to take some time and effort learning this, keep at it!
+    And finally don't forget your ;s
+    Have fun =)
+
+    DISCLAIMER -
+    You can use and modify this code all you like, do whatever you want with it it's yours now!
+
+ */
+
+
 public class Main {
     
     public static void main(String args[]){
 
         // ArrayList version of crops - No size required
-        ArrayList<Crop> cropList = new ArrayList<Crop>();
+        ArrayList<Crop> cropList = new ArrayList<>();
         cropList.add(new Crop("Pumpkin", 3, 10, 3)) ;
         cropList.add(new Crop("Radish", 5, 7, 5)) ;
         cropList.add(new Crop("Aubergine", 9, 4, 4));
@@ -17,7 +36,6 @@ public class Main {
         cropArr[1] = new Crop("Radish", 5, 7, 5);
         cropArr[2] = new Crop("Aubergine", 9, 4, 4);
 
-        Tree tree = new Tree("Apple", 9, 10, 5, 10);
 
         // Uses ArrayList
         System.out.println("ArrayList Implementation - ");
@@ -31,6 +49,8 @@ public class Main {
             cropArr[i].prtStats();
         }
 
+        // Tree test - Declare an object to a variable instead of a place in an array
+        Tree tree = new Tree("Apple", 9, 10, 5, 10);
         tree.prtStats();
         tree.prtHeight();
 
